92b40e4af3a095f6185d5fa28f4bddd5f7a6882d
Agus Prayogi <agus21apy@gmail.com>
2024-02-22 17:12:19 +0700

    [ADD] room type

---
+++ app/Filament/Resources/RoomTypeResource.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\RoomTypeResource\Pages;
+use App\Filament\Resources\RoomTypeResource\RelationManagers;
+use App\Models\RoomType;
+use Filament\Forms;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class RoomTypeResource extends Resource
+{
+    protected static ?string $model = RoomType::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->label('Nama')
+                    ->required(),
+                TextInput::make('price')
+                    ->numeric()
+                    ->label('Harga'),
+                TextInput::make('capacity')
+                    ->label('Kapasitas')
+                    ->numeric()
+                    ->required(),
+                Textarea::make('note')
+                    ->label('Catatan')
+                    ->required(),
+                Toggle::make('is_recommended')
+                    ->label('Rekomendasi')
+                    ->required()
+                    ->default(true),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nama')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('price')
+                    ->label('Harga')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('capacity')
+                    ->label('Kapasitas')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\BooleanColumn::make('is_recommended')
+                    ->label('Rekomendasi')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('note')
+                    ->label('Catatan')
+                    ->searchable()
+                    ->sortable(),
+
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRoomTypes::route('/'),
+            'create' => Pages\CreateRoomType::route('/create'),
+            'edit' => Pages\EditRoomType::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}

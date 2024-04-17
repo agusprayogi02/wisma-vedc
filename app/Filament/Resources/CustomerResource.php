b107fa5ce873d32948bc52d1de1e37ccff3d88bd
Agus Prayogi <agus21apy@gmail.com>
2024-04-17 16:15:15 +0700

    [ADD] customer validation

--- app/Filament/Resources/CustomerResource.php
+++ app/Filament/Resources/CustomerResource.php
@@ -3,13 +3,15 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\CustomerResource\Pages;
-use App\Filament\Resources\CustomerResource\RelationManagers;
 use App\Models\Customer;
-use Faker\Provider\ar_EG\Text;
-use Filament\Forms;
+use App\Models\Reservation;
+use App\Models\Room;
+use Closure;
+use Filament\Forms\Components\Component;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
@@ -29,14 +31,27 @@ class CustomerResource extends Resource
         return $form
             ->schema([
                 Select::make('reservation_id')
-                    ->label('Reservation')
-                    ->relationship('reservation', 'orderer_id')
+                    ->label('Pemesan')
+                    ->relationship('reservation.orderer', 'name')
                     ->required()
                     ->preload()
                     ->searchable(),
                 Select::make('room_id')
-                    ->label('Room')
+                    ->label('Ruang')
                     ->relationship('room', 'code')
+                    ->rules([
+                        fn(Get $get, Component $component): Closure =>
+                        function (string $attr, $value, Closure $fail) use ($get, $component) {
+                            $room = Room::find($value)->with('roomType')->first();
+                            $count = Customer::where([
+                                ['reservation_id', '=', $get('reservation_id')],
+                                ['room_id', '=', $value],
+                            ])->count();
+                            if ($count > $room->roomType->capacity) {
+                                $fail('Kamar ini sudah penuh!');
+                            }
+                        }
+                    ])
                     ->required()
                     ->preload()
                     ->searchable(),
@@ -46,7 +61,25 @@ class CustomerResource extends Resource
                 Select::make('gender')->options([
                     'P' => 'Perempuan',
                     'L' => 'Laki-laki',
-                ])->required()->label('Jenis Kelamin'),
+                ])
+                    ->rules([
+                        fn(Get $get, Component $component): Closure =>
+                        function (string $attr, $value, Closure $fail) use ($get, $component) {
+                            $existCustomer = Customer::where([
+                                ['reservation_id', '=', $get('reservation_id')],
+                                ['room_id', '=', $get('room_id')],
+                            ])->first();
+                            $genders = [
+                                'P' => 'Perempuan',
+                                'L' => 'Laki-laki',
+                            ];
+                            if ($existCustomer->exists()) {
+                                if ($existCustomer->gender !== $value) {
+                                    $fail('Kamar ini untuk ' . $genders[$existCustomer->gender]);
+                                }
+                            }
+                        }
+                    ])->required()->label('Jenis Kelamin'),
                 TextInput::make('address')
                     ->label('Alamat')
                     ->required(),
@@ -60,11 +93,11 @@ class CustomerResource extends Resource
     {
         return $table
             ->columns([
-                TextColumn::make('reservation_id.orderer.name')
+                TextColumn::make('reservation.orderer.name')
                     ->label('Orderer')
                     ->searchable()
                     ->sortable(),
-                TextColumn::make('room_id.code')
+                TextColumn::make('room.code')
                     ->label('Room')
                     ->searchable()
                     ->sortable(),
